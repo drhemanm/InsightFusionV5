7aae92e8eb5809012ef5c48330e0ab3a373df588
drhemanm <heman.m@iafrica.solutions>
2025-09-10 11:05:16 +0400

    Remove Trusted by Industry Leaders section from hero page

--- src/components/auth/LandingPage.tsx
+++ src/components/auth/LandingPage.tsx
@@ -208,13 +208,3 @@ export const LandingPage: React.FC = () => {
           </div>
         </div>
       </div>
-
-      {/* Background Decorations */}
-      <div className="absolute inset-0 overflow-hidden pointer-events-none">
-        <div className="absolute -top-40 -right-40 w-80 h-80 bg-blue-500 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse"></div>
-        <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-purple-500 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse delay-1000"></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-indigo-500 rounded-full mix-blend-multiply filter blur-xl opacity-10 animate-pulse delay-500"></div>
-      </div>
-    </div>
-  );
-};
