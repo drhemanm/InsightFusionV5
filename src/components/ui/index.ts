76e3edc3e56b4cab7946bed1216a9cc20cd565a1
drhemanm <heman.m@iafrica.solutions>
2025-10-15 12:57:32 +0400

    Create index.ts

---
+++ src/components/ui/index.ts
@@ -0,0 +1,27 @@
+// Export all UI components from a single file
+export { Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { Input } from './Input';
+export type { InputProps } from './Input';
+
+export { Card, CardHeader, CardBody, CardFooter } from './Card';
+export type { CardProps, CardHeaderProps } from './Card';
+
+export { Modal } from './Modal';
+export type { ModalProps } from './Modal';
+
+export { Dropdown } from './Dropdown';
+export type { DropdownProps, DropdownOption } from './Dropdown';
+
+export { Badge } from './Badge';
+export type { BadgeProps } from './Badge';
+
+export { Avatar, AvatarGroup } from './Avatar';
+export type { AvatarProps, AvatarGroupProps } from './Avatar';
+
+export { Spinner, Skeleton } from './Spinner';
+export type { SpinnerProps, SkeletonProps } from './Spinner';
+
+export { Toast, ToastContainer } from './Toast';
+export type { ToastProps, ToastContainerProps } from './Toast';
